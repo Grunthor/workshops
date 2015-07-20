9088791e7619b00e4c2bb12c4d1cf46bffd375d3
Grunthor <grunek1@gmail.com>
2015-07-20 20:30:13 +0000

    Fixing registration error

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
   
-  def create
-    User.create(sign_up_params)
-  end
+  #def create
+    #User.create(sign_up_params)
+  #end
   
   def sign_up_params
     params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
   end
+  
 end
