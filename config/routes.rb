65c009e76eebd3653e110bb0a21f32a4d62837fd
Grunthor <grunek1@gmail.com>
2015-07-20 12:53:14 +0000

    routes change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
- # devise_for :users, :controllers => {registrations: 'registrations'}
-  devise_for :users, :controllers => {registrations: "devise/registrations"}
+  devise_for :users, :controllers => {registrations: 'registrations'}
   resources :categories do
     resources :products do
       resources :reviews
