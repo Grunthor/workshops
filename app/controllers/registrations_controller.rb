7d2ab8599527544953c376edfc120b9d6f8e7855
Grunthor <grunek1@gmail.com>
2015-07-20 20:16:20 +0000

    Link addition

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
+  
+  def create
+    User.create(sign_up_params)
+  end
+  
   def sign_up_params
     params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
   end
